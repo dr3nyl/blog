76cc86ea2dfb3ddcfa68d1836ea53dcd2a0a9da2
Drenyl <drenyltobi@gmail.com>
2022-03-29 12:01:32 +0800

    Add password hashing by using mutator

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -18,9 +18,9 @@ class RegisterController extends Controller
         $attr = request()->validate([
 
             "name" => ['required', 'max:255'],
-            "username" => ['required', 'max:255', 'min:8'],
+            "username" => ['required', 'max:255'],
             "email" => ['required', 'email', 'max:255'],
-            "password" => ['required', 'min:7', 'max:255']
+            "password" => ['required', 'min:5', 'max:255']
         ]);
 
         
